f354945301a8c917a6ac7bf9d1766e5afd7c0427
hirakiuc <hirakiuc@gmail.com>
2019-05-21 23:39:04 +0900

    Improve finding repository.

--- internal/subcommand/repo/help.go
+++ internal/subcommand/repo/help.go
@@ -2,15 +2,22 @@ package repo
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"strings"
 
 	aw "github.com/deanishe/awgo"
+	"github.com/hirakiuc/alfred-github-workflow/internal/api"
+	"github.com/hirakiuc/alfred-github-workflow/internal/cache"
+	"github.com/hirakiuc/alfred-github-workflow/internal/model"
 )
 
 // HelpCommand describe a subcommand to show the repo subcommand.
 type HelpCommand struct {
 	Owner string
 	Repo  string
+
+	Limit int
 }
 
 // NewHelpCommand return an instance of this subcommand.
@@ -18,6 +25,7 @@ func NewHelpCommand(owner string, repo string) HelpCommand {
 	return HelpCommand{
 		Owner: owner,
 		Repo:  repo,
+		Limit: 50,
 	}
 }
 
@@ -36,8 +44,42 @@ func (cmd HelpCommand) htmlURL(name string) string {
 	return strings.Join(components, "/")
 }
 
-// Run start this subcommand
-func (cmd HelpCommand) Run(_ctx context.Context, wf *aw.Workflow) {
+func (cmd HelpCommand) fetchRepos(ctx context.Context, wf *aw.Workflow) ([]model.Repo, error) {
+	store := cache.NewReposCache(wf)
+
+	repos, err := store.GetCache(cmd.Owner)
+	if err != nil {
+		return []model.Repo{}, err
+	}
+
+	if len(repos) != 0 {
+		return repos, nil
+	}
+
+	client := api.NewClient(ctx)
+	repos, err = client.FetchReposByOwner(ctx, cmd.Owner)
+	if err != nil {
+		return []model.Repo{}, err
+	}
+
+	return store.Store(cmd.Owner, repos)
+}
+
+func findReposContains(repos []model.Repo, key string) []model.Repo {
+	keyword := strings.ToUpper(key)
+
+	ret := []model.Repo{}
+	for _, repo := range repos {
+		if strings.Contains(strings.ToUpper(repo.Name), keyword) == true {
+			ret = append(ret, repo)
+		}
+	}
+
+	return ret
+}
+
+func (cmd HelpCommand) appendSubCommand(wf *aw.Workflow) {
+	// Show subcommands if the repo found.
 	subcommands := []struct {
 		name string
 		desc string
@@ -67,4 +109,44 @@ func (cmd HelpCommand) Run(_ctx context.Context, wf *aw.Workflow) {
 			Arg(cmd.htmlURL(sub.name)).
 			Valid(true)
 	}
+}
+
+// Run start this subcommand
+func (cmd HelpCommand) Run(ctx context.Context, wf *aw.Workflow) {
+	repos, err := cmd.fetchRepos(ctx, wf)
+	if err != nil {
+		wf.FatalError(err)
+		return
+	}
+
+	founds := findReposContains(repos, cmd.Repo)
+	for _, repo := range founds {
+		fmt.Fprintf(os.Stderr, "found:%s\n", repo.Name)
+	}
+
+	if len(founds) == 1 {
+		repo := founds[0]
+		if strings.ToUpper(repo.Name) == strings.ToUpper(cmd.Repo) {
+			// show subcommand if found exactly one repo.
+			cmd.appendSubCommand(wf)
+		} else {
+			// show the repo if found but not exactly same name.
+			wf.NewItem(repo.Name).
+				Subtitle(repo.Description).
+				Autocomplete(cmd.Owner + "/" + repo.Name + " ").
+				Arg(repo.HTMLURL).
+				Valid(true)
+		}
+	} else {
+		// show repos if found some repos.
+		for _, repo := range founds {
+			wf.NewItem(repo.Name).
+				Subtitle(repo.Description).
+				Autocomplete(cmd.Owner + "/" + repo.Name + " ").
+				Arg(repo.HTMLURL).
+				Valid(true)
+		}
+	}
+
+	wf.WarnEmpty("No such repo found.", "")
 }
